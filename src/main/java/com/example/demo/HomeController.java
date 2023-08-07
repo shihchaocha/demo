8a4f670f7c794591dd1dc4ceabfda2d4124f602a
CSC <shichoc@gmail.com>
2023-08-07 14:00:06 +0800

    do something

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-//test +++
+//test
 @RestController
 public class HomeController {
 
