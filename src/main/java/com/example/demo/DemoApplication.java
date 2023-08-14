c710fc1b714a5997a7e80749e0ea56a01c9fdc57
shichoc <108203158+shihchaocha@users.noreply.github.com>
2023-08-14 10:13:37 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//test
 @SpringBootApplication
 public class DemoApplication {
 
